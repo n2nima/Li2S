083a3baaaf1b8bcf5c265351bd178773d6203a08
n2nima <nima.najdi@gmail.com>
2023-12-15 02:35:24 +0330

    final works

--- func/_config.sql
+++ func/_config.sql
@@ -3,20 +3,23 @@ CREATE DATABASE Li2S;
 
 -- create USER table
 -- insert it in DB selected range
-CREATE TABLE USERS(
-    ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    USER_NAME VARCHAR(50) NOT NULL,
-    EMAIL VARCHAR(100) NOT NULL,
-    USER_PASS VARCHAR(255) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `first_name` varchar(50) NOT NULL,
+    `last_name` varchar(50) NOT NULL,
+    `username` varchar(50) NOT NULL,
+    `email` varchar(50) NOT NULL,
+    `password` varchar(50) NOT NULL,
+    `create_datetime` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+   );
 
 -- create URL table
 -- insert it in DB selected range
-CREATE TABLE URL(
-    ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    USER_ID INT(6),
-    FULL_URL VARCHAR(255) NOT NULL,
-    SHORT_URL VARCHAR(100) NOT NULL,
-    USER_PASS VARCHAR(255) NOT NULL,
-    CLICKS INT(6)
+CREATE TABLE IF NOT EXISTS `urls`(
+    `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT(6),
+    `full_url` VARCHAR(255) NOT NULL,
+    `short_url` VARCHAR(100) NOT NULL,
+    `clicks` INT(6)
 );
